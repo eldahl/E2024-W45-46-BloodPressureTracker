e832c8a4bc745893d89160c09603f3aa3baa8a8a
eldahl <71466904+eldahl@users.noreply.github.com>
2024-11-12 04:11:11 +0100

    Revert last test commit

--- Version-1/Models/BtpDbContext.cs
+++ Version-1/Models/BtpDbContext.cs
@@ -7,16 +7,16 @@ public class BtpDbContext : DbContext
 {
     public virtual DbSet<Measurement> Measurements { get; set; }
     public virtual DbSet<Patient> Patients { get; set; }
-    
-    public BtpDbContext(){}
+
+    public BtpDbContext() { }
     public BtpDbContext(DbContextOptions<BtpDbContext> options) : base(options) { }
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         string connectionString = Environment.GetEnvironmentVariable("MySQLDbConnection")!;
         optionsBuilder.UseMySQL(connectionString ?? throw new NullReferenceException("Environment variable MySQLDbConnection not supplied...!"));
     }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         var dateOnlyToFromDateTimeConverter = new ValueConverter<DateOnly, DateTime>(
@@ -24,8 +24,6 @@ public class BtpDbContext : DbContext
             dateTimeValue => DateOnly.FromDateTime(dateTimeValue)
         );
 
-        var test = 1;
- 
         modelBuilder.Entity<Measurement>(entity =>
         {
             // Primary key | Identifier of measurement
@@ -33,7 +31,7 @@ public class BtpDbContext : DbContext
             entity.Property(p => p.Id)
                 .ValueGeneratedOnAdd()
                 .IsRequired();
-            
+
             entity.Property(p => p.Date)
                 .IsRequired()
                 .HasConversion(dateOnlyToFromDateTimeConverter);
@@ -41,50 +39,50 @@ public class BtpDbContext : DbContext
             entity.Property(p => p.Diastolic).IsRequired();
             entity.Property(p => p.Seen);
         });
-        
+
         // Should an index be added? No, this is done automatically for foreign keys in InnoDB.
         // https://stackoverflow.com/questions/304317/does-mysql-index-foreign-key-columns-automatically
         modelBuilder.Entity<Measurement>()
             .HasOne(m => m.Patient)
             .WithOne()
             .HasForeignKey<Measurement>(m => m.PatientSsn);
-        
+
         modelBuilder.Entity<Patient>(entity =>
         {
             // Primary key | Social Security Number of patient
             entity.HasKey(p => p.SSN);
             entity.Property(p => p.SSN).IsRequired();
-            
+
             entity.Property(p => p.Mail).IsRequired();
             entity.Property(p => p.Name).IsRequired();
         });
 
         modelBuilder.Entity<Patient>().HasData(
-            new Patient 
+            new Patient
             {
                 SSN = "462-59-4864",
                 Name = "Wilber Ma",
                 Mail = "WMA@mail.com"
             },
-            new Patient 
+            new Patient
             {
                 SSN = "144-75-2929",
                 Name = "Rebeca Pao",
-                Mail = "PAO@mail.com" 
+                Mail = "PAO@mail.com"
             },
-            new Patient 
+            new Patient
             {
                 SSN = "178-14-0036",
                 Name = "Raghallach Husson",
                 Mail = "RHusson@mail.com"
             },
-            new Patient 
+            new Patient
             {
                 SSN = "500-29-2239",
                 Name = "Xena Yun",
                 Mail = "xy@mail.com"
             },
-            new Patient 
+            new Patient
             {
                 SSN = "509-90-5304",
                 Name = "Helana Clayton",
@@ -94,7 +92,7 @@ public class BtpDbContext : DbContext
 
         DateOnly firstDayOf2024 = DateOnly.FromDateTime(new DateTime(2024, 1, 1));
         modelBuilder.Entity<Measurement>().HasData(
-            new Measurement 
+            new Measurement
             {
                 Id = 1,
                 Date = firstDayOf2024,
@@ -103,7 +101,7 @@ public class BtpDbContext : DbContext
                 Seen = false,
                 PatientSsn = "462-59-4864"
             },
-            new Measurement 
+            new Measurement
             {
                 Id = 2,
                 Date = firstDayOf2024,
@@ -112,7 +110,7 @@ public class BtpDbContext : DbContext
                 Seen = false,
                 PatientSsn = "144-75-2929"
             },
-            new Measurement 
+            new Measurement
             {
                 Id = 3,
                 Date = firstDayOf2024,
@@ -121,7 +119,7 @@ public class BtpDbContext : DbContext
                 Seen = false,
                 PatientSsn = "178-14-0036"
             },
-            new Measurement 
+            new Measurement
             {
                 Id = 4,
                 Date = firstDayOf2024.AddDays(1),
@@ -130,7 +128,7 @@ public class BtpDbContext : DbContext
                 Seen = false,
                 PatientSsn = "500-29-2239"
             },
-            new Measurement 
+            new Measurement
             {
                 Id = 5,
                 Date = firstDayOf2024.AddDays(1),
@@ -140,7 +138,7 @@ public class BtpDbContext : DbContext
                 PatientSsn = "509-90-5304"
             }
         );
-        
-        
+
+
     }
 }
